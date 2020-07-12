68510f11e2b6bdf4749ac4ae369affc802b4d92a
EduardoGiacomini <carloseduardo.diasgiacomini@gmail.com>
2020-07-12 11:11:50 -0400

    feat: criei o primeiro teste para o módulo core

---
+++ server/src/core/index.js
@@ -0,0 +1,13 @@
+class Core {
+    start() {
+        console.log('[core] Starting...');
+        console.log('[core] Started!');
+    }
+
+    stop() {
+        console.log('[core] Stopping...');
+        console.log('[core] Stopped!');
+    }
+}
+
+module.exports = Core;
