b3474fc0957aa90ba9a7f64175197eeb54f29f7b
EduardoGiacomini <carloseduardo.diasgiacomini@gmail.com>
2020-07-26 21:28:22 -0400

    feat: adicionei o vuetify

--- application/src/components/HelloWorld.vue
+++ application/src/components/HelloWorld.vue
@@ -1,7 +1,19 @@
 <template>
-    <div>
-        <h1>Hello World</h1>
-    </div>
+  <v-container>
+    <v-row class="text-center">
+      <v-col cols="12">
+        <v-img
+          :src="require('../assets/logo.svg')"
+          class="my-3"
+          contain
+          height="200"
+        />
+      </v-col>
+      <v-col class="mb-4">
+        <h1 class="display-2 font-weight-bold mb-3">Hello World</h1>
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
 
 <script>
